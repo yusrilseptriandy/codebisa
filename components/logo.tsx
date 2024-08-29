33d09c878196ca5b5dd0340dc42ef445b9eab865
Yusril Septriandy. N. Y <yusrilseptriandy@gmail.com>
2024-08-29 23:58:24 +0700

    change image logo

--- components/logo.tsx
+++ components/logo.tsx
@@ -9,6 +9,7 @@ import {
 } from 'next/font/google';
 import { FaChevronLeft, FaChevronRight } from 'react-icons/fa6';
 import Link from 'next/link';
+import Image from 'next/image';
 
 const font = Fraunces({ subsets: ['latin'], weight: '400' });
 const font2 = Tac_One({ subsets: ['latin'], weight: '400' });
@@ -16,9 +17,13 @@ const Logo = () => {
   return (
     <Link href={'/'} className={`${font.className} text-2xl flex items-center`}>
       <div className=" flex items-center text-white bg-sky-600 w-12 h-12 justify-center rounded-full">
-        <FaChevronLeft className="-m-2 " />
-        <p className={`${font2.className} text-3xl`}>?</p>
-        <FaChevronRight className="-m-2" />
+        <Image
+          src={'/logo.jpg'}
+          width={50}
+          height={50}
+          alt="logo"
+          className="rounded-lg"
+        />
       </div>
       <h1 className="font-semibold mr-1 text-md ml-3">Codebisa</h1>
     </Link>
