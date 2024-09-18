fa08a3449d50a15ded2e8ec319e73c57d01cf7c6
Yusril Septriandy. N. Y <yusrilseptriandy@gmail.com>
2024-09-18 23:21:14 +0700

    change logo name

--- app/(page)/projects/page.tsx
+++ app/(page)/projects/page.tsx
@@ -1,5 +1,17 @@
+import Image from 'next/image';
+
 const ProjectsPage = () => {
-  return <div>Project page</div>;
+  return (
+    <div className="w-full h-full flex items-center justify-center">
+      <Image
+        src={'/cs.jpg'}
+        alt="coming soon"
+        width={800}
+        height={500}
+        className=""
+      />
+    </div>
+  );
 };
 
 export default ProjectsPage;
